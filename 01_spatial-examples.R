1a95cef96f5d2509347b8b69bc0127d1cd2273cc
Rob Schick <robschick@gmail.com>
2019-10-10 08:41:22 -0400

    First Check-in

---
+++ 01_spatial-examples.R
@@ -0,0 +1,10 @@
+library(mdsr)
+library(sp)
+
+# Let's plot out the locations of deaths due to Cholera
+# These were made famous by John Snow in the mid 1800's
+plot(CholeraDeaths)
+
+# what does that tell us? N
+
+head(CholeraDeaths)
